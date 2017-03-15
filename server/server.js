8eb76b9c21b7244c1572c8ffcefa835793558545
krishnakandula <krishna.c.kandula@gmail.com>
2017-03-15 11:38:49 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -11,7 +12,7 @@ let app = express();
 app.use(bodyParser.json());
 
 //Creating a new todo
-app.post('/todos', (req, res) => {
+app.post('/todos', (req, res) => { 
     let todo = new Todo({
         text: req.body.text
     });
@@ -30,6 +31,31 @@ app.get('/todos', (req, res) => {
         });
     }, (error) => {
         res.status(400).send(error);
+    });
+});
+
+//Gets individual todo
+//GET /todos/:id
+//url parameter
+app.get('/todos/:id', (req, res) => {
+    //Key value pairs
+    //key is id, value is id value
+    let id = req.params.id;
+
+    //404 - send back empty body
+    if(!ObjectID.isValid(id)){
+        res.status(404).send(`Invalid id: ${id}`);
+    }
+    
+    //findbyid
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.send(`Todo with id: ${id} not found`);
+        }
+
+        res.status(200).send({todo});
+    }, (error) => {
+        res.status(400).send();
     });
 });
 
