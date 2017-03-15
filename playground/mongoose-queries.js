165d4541955a1079275ff5a9c896a47d637f26be
krishnakandula <krishna.c.kandula@gmail.com>
2017-03-15 10:04:14 -0500

    Add ObjectID check in playground

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,8 +1,14 @@
+const {ObjectID} = require('mongodb');
+
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
 
-let id = '58c74bc3e34d6c1f2848035b';
+// let id = '58c74bc3e34d6c1f2848035b';
 
+// if(!ObjectID.isValid(id)){
+//     console.log('Id not valid');
+// }
 // Todo.find({
 //     //No need to convert id to ObjectId, mongoose does for us
 //     _id: id
@@ -17,11 +23,24 @@ let id = '58c74bc3e34d6c1f2848035b';
 //     console.log('Todo', todo);
 // });
 
-Todo.findById(id).then((todo) => {
-    if(!todo){
+// Todo.findById(id).then((todo) => {
+//     if(!todo){
+//         return console.log('Id not found');
+//     }
+//     console.log('Todo by id', todo);
+// }).catch((error) => {
+//     console.log(error);
+// });
+
+//3 cases
+//1: Query has no errors, user is not found
+//2: Query has no errors, user is found
+//3. Query has errors
+User.findById('58c71022fcc8d618b092ca8e').then((user) => {
+    if(!user){
         return console.log('Id not found');
     }
-    console.log('Todo by id', todo);
-}).catch((error) => {
+    console.log('Todo by id: ', JSON.stringify(user, undefined, 2));
+}, (error) => {
     console.log(error);
 });
