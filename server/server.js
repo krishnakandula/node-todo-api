6d11371a65b38bd5b82073f789fd05ad4ff48fe6
krishnakandula <krishna.c.kandula@gmail.com>
2017-03-19 17:24:35 -0500

    Add POST /users route

--- server/server.js
+++ server/server.js
@@ -113,6 +113,18 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+// POST /users
+app.post('/users', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password']);
+    let user = new User(body);
+
+    user.save().then((user) => {
+        res.status(200).send(user);
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
